8a3ce2af234fd180def6a11dbf29a8bffc9a83ca
perdaf <perdafsteeve@free.fr>
2019-07-16 09:46:05 -0400

    +docker, +/public, +/view, ++index.js

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+const createError = require("http-errors");
 const express = require("express");
-
-require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const logger = require("morgan");
+const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 const routes = require("./routes/routes");
 
+require("dotenv").config();
+const PORT = process.env.NODE_ENV == "development" ? 3000 : process.env.PORT;
+
+app.use(logger("combined"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err));
+
 app.use("/api", routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 app.listen(PORT, () => {
   console.log(`server connect on port ${PORT}`);
---
+++ readme.md
@@ -0,0 +1,12 @@
+## BoilerPlate pour Node, express, mongoDB avec docker
+
+Make sur you have Docker install on your system.
+clone the app.
+In a console tape **docker-compose up** to start.
+docker-compose up -d let you run in background
+
+### todo:
+
+- [ ] Add schema for mongo data
+- [ ] create static pages (error 404, etc...)
+- [ ] tester la bdd et la localisation des fichiers (./data/db:/data/db)
